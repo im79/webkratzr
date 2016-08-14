03bf3216643fa27e09851b8ca00d5c858b0ead2e
Ilya <mordkovich.ilya@googlemail.com>
2016-08-14 10:56:15 +0200

    twitter api encoding problem gefixt

--- app.inc.js
+++ app.inc.js
@@ -16,34 +16,42 @@ module.exports = {
 		request(url, function (error, response, body) {
 			if (!error) {
 
-				var $ = cheerio.load(body, { decodeEntities: false }), headline = $(".hcf-headline").first().html();
+				var $ = cheerio.load(body, {
+					decodeEntities: false }
+				), headline = $(".hcf-headline").first().html();
 
 				console.log("Letzte Nachricht: " + headline + "");
 
 				var translator = require('yandex-translate-api')(config.yandex_api_key);
 				translator.translate(headline, { to: 'en'}, function(err, res) {
+					headlinetrans = res.text;
+					console.log("Letzte Nachricht translated: " + headlinetrans + "");
 
-				  headline = res.text;
-					console.log("Letzte Nachricht translated: " + headline + "");
+
+					var client = new twitter({
+						consumer_key: config.consumer_key,
+						consumer_secret: config.consumer_secret,
+						access_token_key: config.access_token_key,
+						access_token_secret: config.access_token_secret
+					});
+
+
+					var params = {
+						status: headlinetrans.toString("utf8")
+					};
+
+					client.post('statuses/update', params,  function(error, tweet, response){
+						if(error) {
+							console.log("trying to post " + headlinetrans + "");
+							console.log(JSON.stringify(error) );
+						} else{
+							console.log('OK');
+						}
+					});
+
 				});
 
-				var client = new twitter({
-					consumer_key: config.consumer_key,
-					consumer_secret: config.consumer_secret,
-					access_token_key: config.access_token_key,
-					access_token_secret: config.access_token_secret
-				});
-				var params = {
-					screen_name: 'nodejs',
-					status: headline
-				};
-				client.post('statuses/update', params,  function(error, tweet, response){
-					if(error) {
-						console.log(JSON.stringify(error) );
-					} else{
-						console.log('OK');
-					}
-				});
+
 			} else {
 				console.log("We’ve encountered an error: " + error);
 			}
