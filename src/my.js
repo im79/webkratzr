5807b6f8335fbf7a4dac0bdb2cc57c05dcec6750
Ilya <mordkovich.ilya@googlemail.com>
2016-08-16 00:05:07 +0200

    ajax submit

--- src/my.js
+++ src/my.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
     MyApp = {
         appinit: function() {
-            MyApp.DocSidebarToggle();
+          if($("#form-signin").length){ MyApp.LoadLogin(); }
+          MyApp.DocSidebarToggle();
             //more
         },
 
+        LoadLogin: function() {
+          jQuery('#form-signin').live('submit',function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: '/',
+                type: 'POST',
+                dataType: 'json',
+                data: $('#form-signin').serialize(),
+                success: function( data ) {
+                    for(var id in data) {
+                        jQuery('#' + id).html(data[id]);
+                    }
+                }
+            });
+            return false;
+        });
 
+
+        },
         DocSidebarToggle: function() {
-            console.log('Messagte: clean run'); 
+            console.log('Messagte: clean run');
         }
     };
 
